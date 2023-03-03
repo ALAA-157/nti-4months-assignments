1aea8cf4cf585b9d6bf39db6047cbbdc87d846e8
ALAA-157 <alaalolokhaled222@gmail.com>
2023-03-03 09:27:16 +0200

    timer 0 PWM test code

--- AVR/APP/timer0_PWM/main.c
+++ AVR/APP/timer0_PWM/main.c
@@ -13,20 +13,6 @@
 
 
 u8 counter_tick =0;
-//u8 ticks_num=0,timer0_init_value=0;
-/*void delay_ms_timer0(u16 msec)
-{
-	f32 T = (u8)((TIMER0_PRESCALER*256)/F_CPU);
-	if(((msec*0.001) % T)==0)
-	{
-		ticks_num= (u8)((msec*0.001) / T);
-	}
-	else
-	{
-		ticks_num= (u8)((msec*0.001) / T)+1;
-	}
-	timer0_init_value=(u8)(((msec*0.001) % T)*256);
-}*/
 void timer_delay (void)
 {
 	if (counter_tick < 5)
@@ -38,28 +24,14 @@ void timer_delay (void)
 		counter_tick=0;
 		led_toggle(LED_RED);
 		TIMER2_void_SetTimerReg(66);
-		//TIMER0_void_SetTimerReg(66);
 	}
 	
 }
 int main(void)
 {
-	/*led_init ();
-	TIMER2_void_Init();
-	TIMER2_void_SetTimerReg(66);
-	TIMER2_void_SetOVCallBack(timer_delay);
-	global_interrupt_Enable();
-	TIMER2_void_EnableOVInt();*/
+	
 	dio_vidConfigChannel(DIO_PORTB, 3, OUTPUT);
-	//led_init ();
 	TIMER0_void_Init();
-	
-	//TIMER0_void_SetCTCCallBack(timer_delay);
-	
-	//TIMER0_void_EnableCTCInt();
-	//global_interrupt_Enable();
-	
-    /* Replace with your application code */
     while (1) 
     {
 		for(u8 i =0;i<256;i++)
